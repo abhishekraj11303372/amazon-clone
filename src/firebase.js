c0c8a300778bb73c0fa6ff1d62c2ca13bba34072
Abhishek Raj <abhishekraj1130@gmail.com>
2021-12-15 22:37:52 +0530

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJa0RAJRL6G_rIlc2XewbEWAcxXcmukPU",
+    authDomain: "clone-b34c8.firebaseapp.com",
+    projectId: "clone-b34c8",
+    storageBucket: "clone-b34c8.appspot.com",
+    messagingSenderId: "434921713658",
+    appId: "1:434921713658:web:032129dc7302c99ad7fe43",
+    measurementId: "G-47MYE3ZE75"
+  };
