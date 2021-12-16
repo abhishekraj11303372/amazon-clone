da23578142309b13712d0f4d861fcc9a81cc58f3
Abhishek Raj <abhishekraj1130@gmail.com>
2021-12-17 02:10:17 +0530

    Header init

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css'
+
+function header() {
+    return (
+        <div className='header'> 
+            <img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+            <div className='header_search'>
+                <input className='header_searchInput' type='text'/>
+                {/*Logo*/}
+            </div>
+            <div className='header_nav'>
+                <div className='header_option'>
+                    <span className='header_LineOne'>Hello Guest</span>
+                    <span className='header_LineTwo'>Sign In</span>
+                </div>
+                <div className='header_option'>
+                <span className='header_LineOne'>Hello Guest</span>
+                    <span className='header_LineTwo'>Sign In</span>
+                </div>
+                <div className='header_option'>
+                <span className='header_LineOne'>Hello Guest</span>
+                    <span className='header_LineTwo'>Sign In</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
